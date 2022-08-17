73cd27a26263c8f2a06d6d738980d21289ea536a
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-17 18:43:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+function findMatching(drivers, match){
+    const matchedDrivers = drivers.filter(driver =>{
+        if(driver.toLowerCase() == match.toLowerCase()){
+            return true;
+        }
+    })
+
+    return matchedDrivers;
+}
+
+function fuzzyMatch (drivers, letters){
+    const matchedDrivers = drivers.filter(driver =>{
+        for(let i = 0; i < letters.length; i++){
+            if(driver.toLowerCase()[i] !== letters[i].toLowerCase()){
+                return false;
+            }
+        }
+        return true;
+    })
+
+    return matchedDrivers;
+}
+
+function matchName(drivers, name){
+    const matchedDrivers = drivers.filter(driver =>{
+        if(driver.name == name){
+            return true;
+        }
+    })
+
+    return matchedDrivers;
+}
